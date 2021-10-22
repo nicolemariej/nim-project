d7b810cb2e192d3cead7012004fb58c2bc6ce669
Nicole <88035900+nicolemariej@users.noreply.github.com>
2021-10-22 23:19:09 +1000

    Introduction and greetings

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+print("Welcome to NIM")
+print("This is a simplified version of the ancient Chinese game, NIM")
+player1Name = input("Player 1, what is your name? ")
+player1Id = input("Player 1, what is your MIT student ID? ")
+print("Welcome", player1Name)
+player2Name = input("Player 2, what is your name? ")
+player2Id = input("Player 2, what is your MIT student ID? ")
+print("Welcome", player2Name)
+
