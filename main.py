d78d9ff37ede5f5619d51a6a210b85d23f1851a6
Nicole <88035900+nicolemariej@users.noreply.github.com>
2021-10-23 17:28:11 +1000

    Remove invalid entry from inital number of stones

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ player2Id = input("Player 2, what is your MIT student ID? ")
 print("Welcome", player2Name)
 
 #set amount of stones
-
-amountOfStones = int(input(player1Name + " please select a number between 30 - 50 "))
-
+while True:
+    amountOfStones = int(input(player1Name + " please select a number between 30 - 50 "))
+    if amountOfStones in [30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]:
+        break
+    print("The number must be between 30-50")
 #set move
 
 move = int(input("Select how many stones you would like to remove: "))
@@ -27,3 +29,5 @@ amountOfStones = amountOfStones - move
 # show new state
 
 print("There are", amountOfStones, " remaining")
+
+#set player
