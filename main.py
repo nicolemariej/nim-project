fc714c44b8c9d9e660c3ab8371a1a252b1e78bdf
Nicole <88035900+nicolemariej@users.noreply.github.com>
2021-10-23 17:36:33 +1000

    Remove invalid move entries

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ player2 = input("Player 2, what is your name? ")
 player2Name = player2.capitalize()
 player2Id = input("Player 2, what is your MIT student ID? ")
 print("Welcome", player2Name)
+print("Rules of the game:\n", player1Name, "will select a number of stones between 30-50. \nFrom there, both players and the computer will take turns to remove 1-3 stones. \nThe player who removes the final stone wins!\n")
 
 #set amount of stones
 while True:
@@ -19,9 +20,11 @@ while True:
         break
     print("The number must be between 30-50")
 #set move
-
-move = int(input("Select how many stones you would like to remove: "))
-
+while True:
+    move = int(input("Select how many stones you would like to remove: "))
+    if move in [1,2,3]:
+        break
+    print("Invalid move. You must select a number between 1-3")
 # update state
 
 amountOfStones = amountOfStones - move
