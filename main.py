595feef39e2af25c28478a38bd8373f1f724b771
Nicole <88035900+nicolemariej@users.noreply.github.com>
2021-10-23 17:15:27 +1000

    Input amount of stones

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randrange
+#set players
 print("Welcome to NIM")
 print("Hello, I am AI player, please tell me a little bit more about yourself...")
 player1 = input("Player 1, what is your name? ")
@@ -10,4 +12,18 @@ player2Name = player2.capitalize()
 player2Id = input("Player 2, what is your MIT student ID? ")
 print("Welcome", player2Name)
 
+#set amount of stones
+
 amountOfStones = int(input(player1Name + " please select a number between 30 - 50 "))
+
+#set move
+
+move = int(input("Select how many stones you would like to remove: "))
+
+# update state
+
+amountOfStones = amountOfStones - move
+
+# show new state
+
+print("There are", amountOfStones, " remaining")
