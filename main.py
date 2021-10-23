960aee5defefe17c676f2c7911de73be10edc1c4
Nicole <88035900+nicolemariej@users.noreply.github.com>
2021-10-23 12:06:43 +1000

    Input amount of stones

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 print("Welcome to NIM")
-print("This is a simplified version of the ancient Chinese game, NIM")
-player1Name = input("Player 1, what is your name? ")
+print("Hello, I am AI player, please tell me a little bit more about yourself...")
+player1 = input("Player 1, what is your name? ")
+player1Name = player1.capitalize()
 
 player1Id = input("Player 1, what is your MIT student ID? ")
-print("Welcome", player1Name.capitalize())
-player2Name = input("Player 2, what is your name? ")
+print("Welcome", player1Name)
+player2 = input("Player 2, what is your name? ")
+player2Name = player2.capitalize()
 player2Id = input("Player 2, what is your MIT student ID? ")
-print("Welcome", player2Name.capitalize())
+print("Welcome", player2Name)
 
+amountOfStones = int(input(player1Name + " please select a number between 30 - 50 "))
